e19cf280fef963316a7274bc900fa45a23931291
Zeon <zeonstewart23@gmail.com>
2021-08-11 13:48:40 -0400

    commit tree a50ff18fc81766a66ad1e5bfbb6f446d18e70dfa

--- index.js
+++ index.js
@@ -1,28 +1,57 @@
-// Write your solution in this file!
-let employee = {
-    name:'anthony johnson', 
-    streetAddress: '606 Harmony St'
- };
+// // Write your solution in this file!
+// let employee = {
+//     name:'anthony johnson', 
+//     streetAddress: '606 Harmony St'
+//  };
 
-function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return Object.assign({}, employee, {[key] :value});
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+//     return Object.assign({}, employee, {[key] :value});
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {   
+//     //Object.assign({employee}, {[name] :"Sam", [streetAddress] :"12 Broadway"});
+//     employee[key]= value;
+//     return employee;
+// }
+
+// function deleteFromEmployeeByKey(employee, name) {
+//     let newEmployee = {...employee}
+//     delete newEmployee[name];
+//     return newEmployee;
+// }
+
+
+// function destructivelyDeleteFromEmployeeByKey(employee, name) {
+
+//     delete employee[name];
+//     return employee;
+// }
+const employee = {
+    name: 'zeon',
+    streetAddress: "East windsor"
+};
+
+function updateEmployeeWithKeyAndValue(employee, anything, parameter) {
+
+    return {...employee,
+        ... {
+            [anything]: parameter
+        }
+    }
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {   
-    //Object.assign({employee}, {[name] :"Sam", [streetAddress] :"12 Broadway"});
-    employee[key]= value;
-    return employee;
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, anything, parameter) {
+    employee[anything] = parameter;
+    return employee
 }
 
-function deleteFromEmployeeByKey(employee, name) {
-    let newEmployee = {...employee}
-    delete newEmployee[name];
+function deleteFromEmployeeByKey(employee, anything) {
+    const newEmployee = {...employee };
+    delete newEmployee[anything];
     return newEmployee;
 }
 
-
-function destructivelyDeleteFromEmployeeByKey(employee, name) {
-    
-    delete employee[name];
+function destructivelyDeleteFromEmployeeByKey(employee, anything) {
+    delete employee[anything];
     return employee;
 }
